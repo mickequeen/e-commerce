7e95ecfb619239109a5173a47cfdfbda25507d04
pamearteaga <pe_arteaga@hotmail.com>
2018-02-26 00:00:44 -0300

    iniciando react

---
+++ index.js
@@ -0,0 +1,9 @@
+//console.log("hola");
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const container = document.getElementById('app');
+const hola = <h1>lentooooooo!!!</h1>;
+
+ReactDOM.render(hola, app);
