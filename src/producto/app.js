0ef8faaee191d68366e403efe40904996ce8a614
pamearteaga <pe_arteaga@hotmail.com>
2018-02-27 10:41:01 -0300

    gh-pages

--- src/producto/app.js
+++ src/producto/app.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
       for(var i = 0; i < data.length; i++ ) {
       //console.log(`<Product title="${data[i].title}" price="${data[i].price}" image="src/${data[i].img}"/>`)
     }
-  });
+  })
     .fail(function(error) {
       console.log('failed')
     });
